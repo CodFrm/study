e9dd8435d2c7408d9ae31fe4caa35c313800bf6d
CodFrm <code.farmer@qq.com>
2020-08-28 16:47:04 +0800

    反转链表

--- algorithm/leetcode/206.c
+++ algorithm/leetcode/206.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <malloc.h>
 
 //https://leetcode-cn.com/problems/reverse-linked-list/
 //反转链表
@@ -8,45 +9,49 @@ struct ListNode
     struct ListNode *next;
 };
 
-//1 2 3 4
-
 struct ListNode *reverseList(struct ListNode *head)
 {
-    struct ListNode *node = head, *next, *prev = NULL, *tmp;
-    while (node != NULL )
+    if (head == NULL)
     {
-        next = node->next;
-        node->next = prev;
-        if (next == NULL)
-        {
-            head = node;
-            break;
-        }
-        prev = next;
-        tmp = node;
-        head=next;
-        node = next->next;
-        next->next = tmp;
+        return NULL;
+    }
+    struct ListNode *tail = head, *next = head->next, *tmp = NULL;
+    while (next != NULL)
+    {
+        tmp = next->next;
+        tail->next = tmp;
+        next->next = head;
+        head = next;
+        next = tmp;
     }
     return head;
 }
-/*
-struct ListNode* reverseList(struct ListNode* head) {
-    if(head==NULL)
-        return head;
-    struct ListNode *p=head,*temp=head->next,*q=temp;
-    p->next=NULL;
-    while(q)
-    {
-    	q=q->next;
-    	temp->next=p;
-    	p=temp;
-    	temp=q;
-	}
-	return p;
-}
-*/
+
+// a->b->c->d->e
+// b->a->c->d->e
+// c->b->a->d->e
+// c->b->a->d->e
+
 int main()
 {
+    struct ListNode *head = malloc(sizeof(struct ListNode)), *next = malloc(sizeof(struct ListNode));
+    head->val = 1;
+    head->next = next;
+    next->val = 2;
+    next->next = malloc(sizeof(struct ListNode));
+    next = next->next;
+
+    next->val = 3;
+    next->next = malloc(sizeof(struct ListNode));
+    next = next->next;
+
+    next->val = 4;
+    next->next = malloc(sizeof(struct ListNode));
+    next = next->next;
+
+    next->val = 5;
+    next->next = NULL;
+
+    reverseList(head);
     return 0;
 }
